037632fa79cd07a5348c87ebd856a66b0efbc59a
IvanZyf666 <izhang@seisware.com>
2022-04-03 13:10:22 -0600

    refined ui

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react'
 import Book from './Book'
+import { Link } from 'react-router-dom'
 
-class BookShelf extends Component{
-  render(){
+class BookShelf extends Component {
+  render() {
     const { books, shelfFilter } = this.props;
     return (
       <div className="list-books-content">
-        {shelfFilter.map(({shelf, shelfName, index}) => (
+        {shelfFilter.map(({ shelf, shelfName, index }) => (
           <section id={shelf} key={index} className="bookshelf">
-            <h2 className="books-row">{shelfName}</h2>
+            {/* <Link to= className="link">
+              <h2 className="books-row">{shelfName}</h2>
+            </Link> */}
+            <a className="books-row" href={'/#' + shelf}>{shelfName}</a>
             <ol className="books-grid">
-                {Object.values(books).filter(book => book.shelf === shelf).map((book)=>(
-                  <Book key={book.id} book={book} onShelfUpdate={this.props.onShelfUpdate} />
-                ))}
+              {Object.values(books).filter(book => book.shelf === shelf).map((book) => (
+                <Book key={book.id} book={book} onShelfUpdate={this.props.onShelfUpdate} />
+              ))}
             </ol>
           </section>
         ))}
